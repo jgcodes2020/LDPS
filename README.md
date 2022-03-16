213ea01277b7d942235b5811552b9c13ebfbe789
jgcodes2020 <35639051+jgcodes2020@users.noreply.github.com>
2022-03-15 22:20:47 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# LDPS
+LDPS (Local Debian Package System) makes it easy to manage a local repository on Debian-based Linux distributions. It offers a command-line interface in the style of APT, making it easy to use, whoever you may be.
+
+## Notes
+- Currently, the repository is hardcoded to /usr/local/debs.
+- Installation must be done manually.
+  - Copy `ldps` to `/usr/local/bin/ldps`.
+  - Update scripts go to `/etc/ldps/autoupdate.d`
---
+++ update/wezterm.py
@@ -0,0 +1,74 @@
+import re
+import tempfile
+import requests
+from pathlib import Path, PurePath
+import subprocess as subp
+
+DEB_PATH = "/usr/local/debs/wezterm.deb"
+
+def download(url: str, path: Path):
+    r = requests.get(url)
+    with open(path, "wb") as file:
+        for chunk in r.iter_content(chunk_size=1024):
+            file.write(chunk)
+
+
+print("Finding latest wezterm release...")
+
+r = requests.get("https://api.github.com/repos/wez/wezterm/releases/latest",
+    headers = {
+        "Accept": "applications/vnd.github.v3+json"
+    }
+)
+
+dl_asset = next(i for i in r.json()["assets"] if i["name"].endswith("Ubuntu20.04.deb"))
+print(f"Downloading {dl_asset['browser_download_url']}")
+download(dl_asset['browser_download_url'], DEB_PATH)
+
+# PATCHING
+# ============
+
+def append_to_field(field: str, value: str, lines: list[str]):
+    index = next((i for (i, x) in enumerate(lines) if x.startswith(f"{field}:")), None)
+    if index is None:
+        lines.append(f"{field}: {value}\n")
+    else:
+        lines[index] = re.sub(r"\n$", f", {value}\n", lines[index])
+
+def replace_field(field: str, value: str, lines: list[str]):
+    index = next((i for (i, x) in enumerate(lines) if x.startswith(f"{field}:")), None)
+    if index is None:
+        lines.append(f"{field}: {value}\n")
+    else:
+        lines[index] = f"{field}: {value}\n"
+        
+
+with tempfile.TemporaryDirectory() as temp_str:
+    # Unpack deb
+    print(f"Unpacking {Path(DEB_PATH).name} for patching")
+    temp_path = Path(temp_str)
+    subp.run(["dpkg-deb", "-R", DEB_PATH, temp_str]).check_returncode()
+    
+    # Patch control file
+    print(f"Patching {Path(DEB_PATH).name}")
+    ctl_lines = None
+    with open(temp_path / "DEBIAN/control", "r+") as ctl_file:
+        ctl_lines = ctl_file.readlines()
+        append_to_field("Provides", "wezterm", ctl_lines)
+        append_to_field("Conflicts", "wezterm", ctl_lines)
+        append_to_field("Replaces", "wezterm", ctl_lines)
+        ctl_file.truncate(0)
+        ctl_file.seek(0)
+        ctl_file.writelines(ctl_lines)
+    
+    # Repack deb
+    print(f"Repacking {Path(DEB_PATH).name}")
+    subp.run(["dpkg-deb", "-b", temp_str, DEB_PATH]).check_returncode()
+
+print("Done!")
+    
+    
+    
+    
+    
+    
