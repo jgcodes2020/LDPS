392592b2700a4095111cc0594a82646758f2cd01
jgcodes2020 <35639051+jgcodes2020@users.noreply.github.com>
2022-03-16 10:43:43 -0400

    Add some build infrastructure

---
+++ build.sh
@@ -0,0 +1,16 @@
+SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+cd $(dirname $SOURCE)
+
+mkdir -p build/DEBIAN
+cp control/* build/DEBIAN/
+
+mkdir -p build/usr/local/bin
+cp ldps build/usr/local/bin/
+dpkg-deb -b build ldps.deb
